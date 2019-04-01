f82a58444b521e52f3a6b1268c37a1d1a53f8c71
Nakabayashi00 <nktty08@gmail.com>
2019-04-01 16:19:21 +0900

    GW1013追加

---
+++ Models/GW1013/Request/SenyotozakashikoshiIreiHensai.cs
@@ -0,0 +1,14 @@
+﻿namespace WebAPIJsonDataMaker.Models.GW1013.Request
+{
+    public class SenyotozakashikoshiIreiHensai
+    {
+        public int kakuninJikkoKubun { get; set; }
+        public int keiyakushaId { get; set; }
+        public int riyoshaId { get; set; }
+        public int temban { get; set; }
+        public int cifBango { get; set; }
+        public int kamokuCode { get; set; }
+        public int toriatsukaiBango { get; set; }
+        public long hensaiMoshikomiKingaku { get; set; }
+    }
+}
