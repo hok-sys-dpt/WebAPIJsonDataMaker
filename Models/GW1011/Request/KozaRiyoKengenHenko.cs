8a5c3dfe636d830aca10753ed1e833bd487c91fd
nakagawa.saki <mn.qdbp@gmail.com>
2019-05-21 11:33:31 +0900

    shorikubunを削除

--- Models/GW1011/Request/KozaRiyoKengenHenko.cs
+++ Models/GW1011/Request/KozaRiyoKengenHenko.cs
@@ -4,7 +4,6 @@
     {
         public int keiyakushaId { get; set; }
         public int riyoshaId { get; set; }
-        public int shoriKubun { get; set; }
         public int kakuninJikkoKubun { get; set; }
         public KoshinKoza[] KoshinKoza { get; set; }
 
